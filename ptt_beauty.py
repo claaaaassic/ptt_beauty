a480eff465f7998a6358af273db3e82a7500c4a4
claaaaassic <johnny84131@gmail.com>
2016-05-19 17:22:34 +0800

    bug fixed

--- ptt_beauty.py
+++ ptt_beauty.py
@@ -18,8 +18,8 @@ search_range = 10
 def login():
 
     tn = telnetlib.Telnet(HOST)
-    print '連接成功!!!'
-    time.sleep(0.5)
+    print 'connect ptt.cc'
+    time.sleep(2)
     content = tn.read_very_eager().decode('big5','ignore')    #進站畫面   
 
 
@@ -29,21 +29,19 @@ def login():
             print('系統過載請稍候...')
             tn = telnetlib.Telnet('ptt.cc')
             content = tn.read_very_eager().decode('big5','ignore')
-            time.sleep(0.5)
+            time.sleep(2)
 
 
         if u"以 new 註冊:" in content:
             print 'username'
-            # username = raw_input("username：")
             username = account.username()
             tn.write((username+"\r\n").encode('big5'))
             time.sleep(1)
             
             print 'password'
-            # password = raw_input("password：")
             password = account.password()
             tn.write((password+"\r\n").encode('big5'))
-            time.sleep(1.5)
+            time.sleep(6)
             content = tn.read_very_eager().decode('big5','ignore')
 
  
@@ -54,26 +52,26 @@ def login():
         if u"您想刪除其他重複登入的連線嗎" in content:
             print("發現重複連線,刪除他...")
             tn.write("Y\r\n".encode('big5')) 
-            time.sleep(10)
+            time.sleep(20)
             content = tn.read_very_eager().decode('big5','ignore')
 
         if u"任意鍵" in content:
             print("資訊頁面，按任意鍵繼續...")
             tn.write("\r\n".encode('big5'))
-            time.sleep(0.5)
+            time.sleep(2)
             content = tn.read_very_eager().decode('big5','ignore')
 
         if u'編輯器自動復原' in content:
             print('編輯器自動復原：不復原')
             tn.write("q\n".encode('big5'))
-            time.sleep(0.5)
+            time.sleep(2)
             content = tn.read_very_eager().decode('big5','ignore')
 
         if u'要刪除以上錯誤嘗試的記錄嗎' in content:
             print '刪除以上錯誤嘗試的記錄'
             print re.findall('[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+',content)
             tn.write("y\n".encode('big5'))
-            time.sleep(0.5)
+            time.sleep(2)
             content = tn.read_very_eager().decode('big5','ignore')
 
     print '登入成功'
@@ -84,26 +82,26 @@ def login():
 def login_beauty( tn ):
     print '分組討論區'
     tn.write("\r\n".encode('big5') )
-    time.sleep(0.5)
+    time.sleep(2)
     
     print '生活娛樂館'
     tn.write("nnnnnnnnr".encode('big5') )
-    time.sleep(0.5)
+    time.sleep(2)
     
     print '聊天'
     tn.write("nnnnnnnnnnnnnnnnr".encode('big5') )
-    time.sleep(0.5)
+    time.sleep(2)
     
     print '進入beauty'
     tn.write("nr".encode('big5') )
-    time.sleep(0.5)
+    time.sleep(2)
     content = tn.read_very_eager().decode('big5','ignore')
     
 
     while "任意鍵" in content.encode('utf-8'):
         print '請按任意鍵繼續' 
         tn.write("\r\n".encode('big5'))
-        time.sleep(0.5)
+        time.sleep(2)
         content = tn.read_very_eager().decode('big5','ignore')
         print content.encode('utf-8')
 
@@ -111,7 +109,7 @@ def login_beauty( tn ):
 
 def download(contentData):
 
-    for url in contentData.urlList:
+    for url in contentData.searchUrl():
         url = url + '.jpg'
         tmp = re.findall('(?<=imgur.com/)[a-zA-Z0-9]+.jpg',url)
         fname = tmp[0]
@@ -119,8 +117,12 @@ def download(contentData):
         print url
 
         try:
-            res = requests.get( url , stream = True , timeout = 10.0 )         
-        except e :
+            res = requests.get( url , stream = True , timeout = 10.0 )   
+            f = open( fname, 'wb' )
+            shutil.copyfileobj( res.raw , f )   # 寫入圖片
+            f.close()
+            del res      
+        except:
             a , b , c = sys.exc_info()
             log = open(time.strftime('%Y%m%d_%H%M%S') + '.txt','w')     # 例外事件的 log
             log.write(time.strftime('%Y/%m/%d_%H:%M:%S') + '\n')
@@ -128,17 +130,11 @@ def download(contentData):
             log.write('例外訊息 : ' + str(b) + '\n')
             log.write('traceback物件 : ' + str(c) + '\n') 
             log.write('id : ' + str(contentData.id) + '\n')
-            log.write('title : ' + str(contentData.title()) + '\n')
             log.write('url : ' + str(url))
             log.close()
             continue
 
-        f = open( fname, 'wb' )
-        shutil.copyfileobj( res.raw , f )   # 寫入圖片
-        f.close()
-        del res
-
-
+    
 
 def main():
 
@@ -148,14 +144,12 @@ def main():
 
     login_beauty( tn )
 
-    # number_start = int(raw_input("輸入起始頁碼："))
-    # search_range = int(raw_input("輸入結束頁碼："))
-
     tn.write((str(number_start)+"\r\n").encode('big5') )     # 設定初始頁數 跳至第幾項 
-    time.sleep(0.5)
+    time.sleep(2)
     tn.write("r".encode('big5') )  # 進入
-    time.sleep(0.5)
+    time.sleep(2)
     content = tn.read_very_eager().decode('big5','ignore')
+
 
     print 'download now !' 
   
@@ -168,39 +162,38 @@ def main():
         else:
             tn.write("f".encode( 'big5' ))   #往後翻
             # tn.write("b".encode( 'big5' ))   #往前翻
-            time.sleep(0.5)
+            time.sleep(2)
             content = tn.read_very_eager().decode( 'big5' , 'ignore' )   #讀內文
 
          
-        while '請按任意鍵繼續' in content.encode('utf-8'):         # 在刪除文章的情況下預設會跳回目錄，所以輸入 b 會回到進版畫面 
+        while '請按任意鍵繼續' in content.encode('utf-8'):         # 在遇到已刪除文章的情況下預設會跳回目錄，所以輸入 b 會回到進版畫面 
             tn.write( "\nkr".encode( 'big5' ) )                    # \nkr 可以跳到刪除文章的下一篇
-            time.sleep(0.5)
+            time.sleep(2)
             content = tn.read_very_eager().decode( 'big5' , 'ignore' )
         
         
         contentData = ContentData.ContentData(content,i)
-        contentData.contentReplace()        # 去掉奇怪字元
+        contentData.contentReplace()                            # 去掉奇怪字元
         
 
-        for page in range(contentData.searchPage()):       # 翻幾次頁
+        for page in range(contentData.searchPage()):            # 翻幾次頁
             if page == 1:
-                contentData.searchUrl( contentData.content )
+                pass
             else:
                 tn.write( "PgDn".encode('big5') )
-                time.sleep(0.5)
-                contentData.searchUrl( tn.read_very_eager().decode('big5','ignore') )
+                time.sleep(2)
+                contentData.addContent( tn.read_very_eager().decode('big5','ignore') )
 
         download(contentData)
 
     print "log out......" 
 
     tn.write("qqqqqqqqqg\r\ny\r\n".encode('big5') )            #登出
-    time.sleep(0.5)
+    time.sleep(2)
     tn.write("\r\n".encode('big5') )
 
     end = time.time()
     print '運行時間 : ' + str(end-start)
-
 
 
 if __name__ == '__main__' :  
